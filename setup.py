b40b6166215e73ac262826cae3c98e15bbc20878
Albert Suarez <alsumo95@gmail.com>
2020-02-25 20:40:30 +0100

    Testing PyPi upgrading

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as readme_file:
 
 setuptools.setup(
     name='nmslib-viz',
-    version='0.1',
+    version='0.1.1',
     author='AlbertSuarez',
     author_email='alsumo95@gmail.com',
     description='NMSLIB visualization tool',
