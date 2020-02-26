040ae39d703d6323ee8f9fbf8d9d253f8b39a13f
Albert Suarez <alsumo95@gmail.com>
2020-02-26 19:16:53 +0100

    Initialized parameters for view() function

--- nmslib_viz/__init__.py
+++ nmslib_viz/__init__.py
@@ -1,2 +1,5 @@
-def view():
+from nmslib_viz.config import VISUALIZE_MAXIMUM_NODES
+
+
+def view(nmslib_index_file_path, maximum_nodes=VISUALIZE_MAXIMUM_NODES):
     return 'This is the first version of nmslib-viz!'
---
+++ nmslib_viz/config.py
@@ -0,0 +1,6 @@
+VISUALIZE_MAXIMUM_NODES = 100
+
+HELP_NMSLIB_INDEX_FILE_PATH = 'String pointing to the NMSLIB file path.'
+HELP_MAXIMUM_NODES = f'Integer between 1 and {VISUALIZE_MAXIMUM_NODES} representing the amount of nodes to visualize.'
+
+
